d32c0ff5cbf740f8f46b7f793c83a6364504df16
Duygu Yılmaz <duygu.yilmaz2@ogr.gelisim.edu.tr>
2024-11-08 11:39:38 +0300

    Hafta 6

---
+++ hafta6.php
@@ -0,0 +1,59 @@
+<?php
+# Sayıları Alt Alta Yazdırma İşlemi
+// $sayi = 1;
+// while ($sayi < 10) {
+//     echo "Sayı:" . $sayi . "<br>";
+//     $sayi++;
+// }
+# Sayıları Art Arda Toplama İşlemi
+// $sayi = 1;
+// echo $sayi;
+// $toplam = $sayi;
+// while ($sayi < 10) 
+// {
+//     $sayi ++;
+//     $toplam += $sayi;
+//     echo "+". $sayi;   
+// }
+// echo "=". $toplam;
+
+# Do - While Döngüsü
+//$sayi = 1;
+// do 
+// {
+//     // 1 kez çalışıp sonradan koşul ile dönecek kod bloğu
+//     echo $sayi."-- Duygu Yılmaz"."<br>";
+//     $sayi++;
+// }
+// while ($sayi <= 5);
+
+# Bölenleri Yazdırma
+// $sayi = 30;
+// $a = 1;
+// do {
+//     if($sayi%$a==0){
+//         echo $a."sayısı". $sayi."sayısının bir bölenidir."."<br>";
+//     }
+//     $a++;
+// }
+// while($a <= $sayi);
+
+// // Foreach Döngüsü
+// $gunler = array("Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar");
+// foreach($gunler as $gun ){
+//     echo $gun."<br>";
+// }
+
+// $harfler = array("a", "ı", "o", "u", "e", "i", "ö", "ü");
+// $sayi = 0;
+// echo"Alfabedeki Kalın ve İnce Ünlüler Listesi"."<br>";
+// foreach($harfler as $harf){
+//     echo $sayi." : ".$harf."<br>";
+//     $sayi++;
+// }
+
+for ($i = 0; $i <= 10; $i++) {
+    echo"<b>Gelişim Üniversitesi</b><u>Web Tasarım ve Kodlama Programı</u>.<br>";
+}
+
+?>
